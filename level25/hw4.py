dafb0bdbffdf74bc25c88e30e5cc7ae1c4bfbc64
kesanebregvadze <kesanebregvadze@gmail.com>
2024-09-12 22:42:47 +0800

    20 commit

---
+++ level25/hw4.py
@@ -0,0 +1,7 @@
+# დავალება 4:  --> ფერები
+# შექმენი სია 6 ფერით.
+# აიღე მეოთხე და მეხუთე ფერები სიიდან.
+# დაბეჭდე ეს ფერები ეკრანზე.
+colors = ["blue","pink","yello","green","lightblue","Lightgreen",]
+print(colors[4])
+print(colors[5])
