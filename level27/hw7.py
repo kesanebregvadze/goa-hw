d89dc5e0364abc01e92da75bf8de50e6ccfbd908
kesanebregvadze <kesanebregvadze@gmail.com>
2024-09-20 18:39:02 +0400

    27 commit

---
+++ level27/hw7.py
@@ -0,0 +1,7 @@
+# 7)შექმენით ფუნქცია სახელად mathematics და მასში მოახდინეთ მათემატიკური ოპერაციები---[ +  ,  -  , *  ,  //  ]
+def mathimatcs():
+    print(8+8)
+    print(8-8)
+    print(8*8)
+    print(8//8)
+mathimatcs()
