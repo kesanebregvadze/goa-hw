f177411da97892b741a255ae43b1b10972e1a8d8
kesanebregvadze <kesanebregvadze@gmail.com>
2024-11-22 18:27:39 +0400

    27 commit

--- level42/hw/hw.py
+++ level42/hw/hw.py
@@ -1,5 +1,5 @@
 name1 = "giorgisgan"
 name2 = "nini"
 def hapi(name1, name2):
-    return name2, "გილოცავ დაბადების დღეს", name1 ,"დიდიდ სიყვარულით"
-hapi(name1,name2)
+    return f"{name2}, გილოცავ დაბადების დღეს, {name1} ,დიდიდ სიყვარულით"
+print(hapi(name1, name2))
