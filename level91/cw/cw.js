8e8e2d0f992119ae73d3f782f5f437ac11702148
kesanebregvadze <kesanebregvadze@gmail.com>
2025-05-09 12:37:24 +0400

    level88 commit

---
+++ level91/cw/cw.js
@@ -0,0 +1,7 @@
+// level 91:
+// ```
+// 1) ავაგოთ ვებსაიტი სადაც onclic() ის გამოყენებით შევუცვლთ ბექგრაუნდს body tag -ს <3
+// ```
+function backround() {
+    document.body.style.backgroundColor = "lightblue";
+}
---
+++ level91/cw/cw2.js
@@ -0,0 +1,10 @@
+//  ```
+// 2) გამოკლება გავაკეთოთ ფუნქიციის <3 decremet
+// ``` 
+const p = document.getElementById("p")
+let count = 100
+function decremet() {
+    
+    console.log("hdjcnhjcjmnshkdsn")
+
+}
