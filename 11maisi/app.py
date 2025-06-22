9fc3f5ad65932bcb71f6ecb37c1bfd0f901e7e2d
kesanebregvadze <kesanebregvadze@gmail.com>
2025-06-22 12:18:08 +0400

    level104 commit

---
+++ 11maisi/app.py
@@ -0,0 +1,14 @@
+def func():
+    print("გამარჯობა კესანე ")
+func()
+def for_loop_function():
+    for i in range(10):
+        func()
+for_loop_function()
+mixo = input("შეიყვანეთ ციფრი ან რიცხვი:")
+maro = input("შეიყვანეთ ციფრი არ რიცხვი:")
+if mixo > maro:
+    print("მიხოს ფრო მეტი ვაშლი აქვს")
+else:
+    print("მიროს ფრო მეტი ვაშლი აქვს")
+    
---
+++ level97/cw/cw.java
@@ -0,0 +1,5 @@
+// არ გადავაკოპირე!
+puplic class main{
+    public static void main(string[] args)
+      System.out.println("Hello World!") 
+}
